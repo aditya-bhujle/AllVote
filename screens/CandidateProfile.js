75b9af0a78891232ebdc25afd0f061cfe3f064f4
Ivan <IvanZaytsev123@gmail.com>
2020-09-26 15:16:09 -0400

    Cant get this stupid row to align fuck react

--- screens/CandidateProfile.js
+++ screens/CandidateProfile.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import {
-	Image,
-	Platform,
-	StyleSheet,
-	Text,
-	TouchableOpacity,
-	View,
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
 } from "react-native";
 import Constants from "expo-constants";
 import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
@@ -15,244 +15,174 @@ import republicPic from "../assets/images/republican.png";
 import unknownPic from "../assets/images/robot-dev.png";
 
 export default function CandidateProfile(data) {
-	const [candidatePicture, setCandidatePicture] = useState();
-	const newData = data.route.params.data;
+  const [candidatePicture, setCandidatePicture] = useState();
+  const newData = data.route.params.data;
 
-	useEffect(() => {
-		async function determinePicture(jsonObject) {
-			if (jsonObject["twitter picture"] != null) {
-				//console.log(jsonObject["twitter picture"]);
-				setCandidatePicture({ uri: jsonObject["twitter picture"] });
-			} else if (jsonObject["facebook picture"] != null) {
-				let response = await fetch("https://" + jsonObject["facebook picture"]);
-				var json_response = await response.json();
+  useEffect(() => {
+    async function determinePicture(jsonObject) {
+      if (jsonObject["twitter picture"] != null) {
+        //console.log(jsonObject["twitter picture"]);
+        setCandidatePicture({ uri: jsonObject["twitter picture"] });
+      } else if (jsonObject["facebook picture"] != null) {
+        let response = await fetch("https://" + jsonObject["facebook picture"]);
+        var json_response = await response.json();
 
-				//console.log(json_response);
+        //console.log(json_response);
 
-				if (!json_response.data.is_silhouette) {
-					setCandidatePicture({ uri: json_response.data.url });
-				}
-			}
-		}
+        if (!json_response.data.is_silhouette) {
+          setCandidatePicture({ uri: json_response.data.url });
+        }
+      }
+    }
 
-		console.log(newData);
+    switch (newData.party) {
+      case "DEM":
+        setCandidatePicture(democratPic);
+        break;
+      case "REP":
+        setCandidatePicture(republicPic);
+        break;
+      default:
+        setCandidatePicture(unknownPic);
+        break;
+    }
 
-		switch (newData.party) {
-			case "DEM":
-				setCandidatePicture(democratPic);
-				break;
-			case "REP":
-				setCandidatePicture(republicPic);
-				break;
-			default:
-				setCandidatePicture(unknownPic);
-				break;
-		}
-
-		determinePicture(newData);
-	}, [data]);
-
-	return (
-		<View style={styles.body}>
-			<View style={styles.navBar}>
-				<TouchableOpacity onPress={() => data.navigation.goBack()}>
-					<MaterialIcons name="arrow-back" size={24} color="white" />
-				</TouchableOpacity>
-				<MaterialIcons
-					style={styles.menu}
-					name="menu"
-					size={24}
-					color="white"
-				/>
-			</View>
-			<View style={styles.main}>
-				<View style={styles.row}>
-					<Image style={styles.picture} source={candidatePicture}></Image>
-					<View style={styles.rowWrap}>
-						<View style={styles.candidateInfo}>
-							<Text style={styles.candidateName}>
-								{newData.name}
-							</Text>
-							<Text style={styles.contest}>
-								{newData.contest}
-							</Text>
-						</View>
-					</View>
-				</View>
-				<TouchableOpacity onPress={() => data.navigation.goBack()}>
-					<MaterialCommunityIcons
-						style={styles.facebook}
-						name="facebook"
-						size={100}
-						color="white"
-					/>
-				</TouchableOpacity>
-				<TouchableOpacity onPress={() => data.navigation.goBack()}>
-					<MaterialCommunityIcons
-						style={styles.link}
-						name="link-variant"
-						size={100}
-						color="white"
-					/>
-				</TouchableOpacity>
-				<TouchableOpacity onPress={() => data.navigation.goBack()}>
-					<MaterialCommunityIcons
-						style={styles.twitter}
-						name="twitter"
-						size={100}
-						color="white"
-					/>
-				</TouchableOpacity>
-				<TouchableOpacity onPress={() => data.navigation.goBack()}>
-					<MaterialCommunityIcons
-						style={styles.instagram}
-						name="instagram"
-						size={100}
-						color="white"
-					/>
-				</TouchableOpacity>
-				<TouchableOpacity onPress={() => data.navigation.goBack()}>
-					<MaterialCommunityIcons
-						style={styles.thumbsup}
-						name="thumb-up-outline"
-						size={40}
-						color="white"
-					/>
-				</TouchableOpacity>
-				<TouchableOpacity onPress={() => data.navigation.goBack()}>
-					<MaterialCommunityIcons
-						style={styles.thumbsdown}
-						name="thumb-down-outline"
-						size={40}
-						color="white"
-					/>
-				</TouchableOpacity>
-				<Text style={styles.approval}>75%</Text>
-				<TouchableOpacity
-					onPress={() => data.route.params.navigate("BallotScreen")}
-					style={styles.addToBallotBtn}
-				>
-					<Text style={styles.addToBallotBtnText}>Add to Ballot</Text>
-				</TouchableOpacity>
-			</View>
-		</View>
-	);
+    determinePicture(newData);
+  }, [data]);
+  // CAN"t get fuck fuck to allign in row in row view??? wtf this worked earlier line numbers 77 and 78
+  return (
+    <View style={styles.body}>
+      <View style={styles.navBar}>
+        <TouchableOpacity onPress={() => data.navigation.goBack()}>
+          <MaterialIcons name="arrow-back" size={24} color="white" />
+        </TouchableOpacity>
+        <MaterialIcons
+          style={styles.menu}
+          name="menu"
+          size={24}
+          color="white"
+        />
+      </View>
+      <View style={styles.main}>
+        <View style={styles.row}>
+          <Image style={styles.picture} source={candidatePicture}></Image>
+          <View style={styles.rowWrap}>
+            <View style={styles.candidateInfo}>
+              <Text style={styles.candidateName}>{newData.name}</Text>
+              <Text style={styles.contest}>{newData.contest}</Text>
+            </View>
+          </View>
+        </View>
+        <View stlye={styles.row}>
+          <Text style={styles.test}>Fuck Fuck</Text>
+          <Text style={styles.test}>Fuck Fuck</Text>
+        </View>
+      </View>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-	body: {
-		overflow: "hidden",
-		backgroundColor: "#219653",
-		paddingTop: Constants.statusBarHeight,
-		flex: 1,
-		paddingLeft: 22,
-		paddingRight: 22,
-	},
-	navBar: {
-		flexDirection: "row",
-		display: "flex",
-		paddingBottom: 26,
-	},
-	main: {
-		display: "flex",
-		alignItems: "baseline",
-	},
-	menu: {
-		marginLeft: "auto",
-	},
-	row: {
-		flexDirection: "row",
-	},
-	rowWrap: {
-		flexDirection: "row",
-		flexWrap: "wrap",
-	},
-	picture: {
-		width: 100,
-		height: 100,
-		backgroundColor: "#101433",
-		borderRadius: 900,
-	},
-	candidateInfo: {
-		paddingLeft: 20,
-		paddingRight: 100,
-	},
-	candidateName: {
-		fontFamily: "Roboto",
-		fontStyle: "normal",
-		fontWeight: "bold",
-		fontSize: 30,
-		lineHeight: 35,
-		color: "#FFFFFF",
-		paddingBottom: 23,
-	},
-	contest: {
-		fontFamily: "Roboto",
-		fontStyle: "normal",
-		fontWeight: "500",
-		fontSize: 20,
-		lineHeight: 23,
-		color: "#ffffff",
-	},
-	addToBallotBtn: {
-		position: "absolute",
-		width: 163,
-		height: 50,
-		left: 205,
-		top: 170,
-		borderRadius: 8,
-		fontFamily: "Roboto",
-		backgroundColor: "#FFFFFF",
-	},
-	addToBallotBtnText: {
-		position: "absolute",
-		marginLeft: 7,
-		marginTop: 14,
-		width: 150,
-		height: 22,
-		fontFamily: "Roboto",
-		fontStyle: "normal",
-		fontWeight: "bold",
-		fontSize: 18,
-		textAlign: "center",
-		color: "#219653",
-	},
-	facebook: {
-		position: "absolute",
-		marginTop: 200,
-		marginLeft: 225,
-	},
-	link: {
-		position: "absolute",
-		marginTop: 200,
-		marginLeft: 60,
-	},
-	twitter: {
-		position: "absolute",
-		marginTop: 375,
-		marginLeft: 60,
-	},
-	instagram: {
-		position: "absolute",
-		marginTop: 375,
-		marginLeft: 225,
-	},
-	thumbsdown: {
-		position: "absolute",
-		marginLeft: 140,
-		marginTop: 50,
-	},
-	thumbsup: {
-		position: "absolute",
-		marginLeft: 10,
-		marginTop: 50,
-	},
-	approval: {
-		fontFamily: "Roboto",
-		fontStyle: "normal",
-		fontSize: 28,
-		fontWeight: "bold",
-		color: "#ffffff",
-		marginTop: 55,
-		marginLeft: 68,
-	},
+  body: {
+    overflow: "hidden",
+    backgroundColor: "#219653",
+    paddingTop: Constants.statusBarHeight,
+    flex: 1,
+    paddingLeft: 22,
+    paddingRight: 22,
+  },
+  navBar: {
+    flexDirection: "row",
+    display: "flex",
+    paddingBottom: 26,
+  },
+  main: {
+    display: "flex",
+  },
+  menu: {
+    marginLeft: "auto",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "wrap",
+  },
+  rowWrap: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  picture: {
+    width: 100,
+    height: 100,
+    backgroundColor: "#101433",
+    borderRadius: 900,
+  },
+  candidateInfo: {
+    paddingLeft: 20,
+    paddingRight: 100,
+  },
+  candidateName: {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: 30,
+    lineHeight: 35,
+    color: "#FFFFFF",
+    paddingBottom: 23,
+  },
+  contest: {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "500",
+    fontSize: 20,
+    lineHeight: 23,
+    color: "#ffffff",
+  },
+  test: {
+    width: 100,
+  },
+  addToBallotBtn: {
+    width: 163,
+    height: 50,
+    left: 205,
+    top: 170,
+    borderRadius: 8,
+    fontFamily: "Roboto",
+    backgroundColor: "#FFFFFF",
+  },
+  addToBallotBtnText: {
+    marginLeft: 7,
+    marginTop: 14,
+    width: 150,
+    height: 22,
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: 18,
+    textAlign: "center",
+    color: "#219653",
+  },
+  facebook: {
+    marginTop: 200,
+    marginLeft: 225,
+  },
+  link: {
+    marginTop: 200,
+    marginLeft: 60,
+  },
+  twitter: {
+    marginTop: 375,
+    marginLeft: 60,
+  },
+  instagram: {
+    marginTop: 375,
+    marginLeft: 225,
+  },
+  thumbsdown: {},
+  thumbsup: {},
+  approval: {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#ffffff",
+  },
 });
---
+++ screens/MainScreen.js
@@ -0,0 +1,263 @@
+import * as WebBrowser from "expo-web-browser";
+import * as React from "react";
+import Constants from 'expo-constants';
+import {
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Alert, 
+  Linking
+} from "react-native";
+import { stack, useState, useEffect, useCallback } from "react";
+
+import { ScrollView } from "react-native-gesture-handler";
+import { Reminder, CompletedCard, GoToCandidate, ActionItem, Remainder2 } from "../components/HubCard";
+export default function MainScreen(props) {
+  const newDate = new Date();
+  var one_day = 1000 * 60 * 60 * 24
+  const ElectionDay = new Date(newDate.getFullYear(), 11, 3);
+  var Result = Math.round(ElectionDay.getTime() - newDate.getTime()) / (one_day);
+  var Final_Result = Result.toFixed(0) - 30;
+  const [showComponent1, setShowComponent1] = useState(false);
+  const [showComponent2, setShowComponent2] = useState(false);
+  const [showComponent3, setShowComponent3] = useState(false);
+  const [isSelected1, setSelection1] = useState(false);
+  const [isSelected2, setSelection2] = useState(false);
+  const [isSelected3, setSelection3] = useState(false);
+
+
+
+  return (
+    <ScrollView style={styles.scrollView}>
+      <View style={styles.container}>
+        <Text style={styles.header}>Welcome To AllVote.</Text>
+        <Text style={styles.subheader}>{props.route.params.location.county}, {props.route.params.location.city}, NC</Text>
+        <Text style={styles.days}>{Final_Result} days till Election Day!</Text>
+        <Text style={styles.header2}>Action Items</Text>
+        <ActionItem
+          checked1={isSelected1, showComponent1}
+          onToggle1={(() => setShowComponent1(showComponent1 ? true : false)) && (() => setSelection1(isSelected1 ? false : true))}
+          checked2={isSelected2, showComponent2}
+          onToggle2={(() => setShowComponent2(showComponent2 ? true : false)) && (() => setSelection2(isSelected2 ? false : true))}
+          checked3={isSelected3, showComponent3}
+          onToggle3={(() => setShowComponent3(showComponent3 ? true : false)) && (() => setSelection3(isSelected3 ? false : true))}
+        />
+        <Text style={styles.header2}>Learn</Text>
+        <GoToCandidate
+          box_style={styles.card4}
+          buttonText_color={styles.VotingButtonText}
+          title="Current Voting Situation"
+          content="Learn more about how COVID-19 has impacted your ability to vote."
+          buttonText="Learn More"
+          onPress={() => Linking.openURL("https://www.ucsusa.org/resources/voting-and-covid-19")}
+        />
+
+        {showComponent1 ? (
+          <CompletedCard
+            box_style={styles.CompletedCard}
+            button_text_color={styles.CompletedbuttonText}
+            title="Hurray, you've registered to vote!"
+            buttonText="Undo"
+            onPress={() => setShowComponent1(showComponent1 ? false : true) && (() => setSelection1(isSelected1 ? false : true))}
+          />
+        ) :
+          <Reminder
+            URL="https://www.ncdot.gov/dmv/offices-services/online/Pages/voter-registration-application.aspx"
+            box_style={styles.card}
+            buttontext1_color={styles.buttonText}
+            buttontext2_color={styles.buttonText}
+            title="Set-up Voting for Election Day!"
+            content="Check that you’re elligible to vote, learn how to register, and learn where to go."
+            buttonText1="Find out how to register"
+            buttonText2="I've completed this"
+            onPress2={() => setShowComponent1(showComponent1 ? false : true) && (() => setSelection1(isSelected1 ? false : true))}
+          />
+        }
+        {showComponent2 ? (
+          <CompletedCard
+            box_style={styles.CompletedCard}
+            button_text_color={styles.CompletedbuttonText}
+            title="Awesome, now your ballot is on the way!"
+            buttonText="Undo"
+            onPress={() => setShowComponent2(showComponent2 ? false : true) && (() => setSelection2(isSelected2 ? false : true))}
+          />
+
+        ) :
+          <Reminder
+            URL="https://www.ncsbe.gov/Voting-Options/Absentee-Voting"
+            box_style={styles.card2}
+            buttontext1_color={styles.buttonText2}
+            buttontext2_color={styles.buttonText2}
+            title="Request a ballot"
+            content="One step closer to casting a vote for your favorite candidate! Learn how to request a mail-in ballot."
+            buttonText1="Request a ballot"
+            buttonText2="I've completed this"
+            onPress2={() => setShowComponent2(showComponent2 ? false : true) && (() => setSelection2(isSelected2 ? false : true))}
+
+          /> 
+        }
+        
+        {showComponent3 ? (
+
+          <CompletedCard
+            box_style={styles.CompletedCard}
+            button_text_color={styles.CompletedbuttonText}
+            title="Hurray, you've made a practice ballot!"
+            buttonText="Undo"
+            onPress={() => setShowComponent3(showComponent3 ? false : true) && (() => setSelection3(isSelected3 ? false : true))}
+          />
+        ) :
+          <Remainder2
+            box_style={styles.card3}
+            buttontext1_color={styles.buttonText3}
+            buttontext2_color={styles.buttonText3}
+            title="Make a practice ballot"
+            content="One step closer to casting a vote for your favorite candidate! Learn how to request a mail-in ballot."
+            buttonText1="Practice Ballot"
+            buttonText2="I've completed this"
+            onPress1={() => props.route.params.nav.navigate("Candidates")}
+            onPress2={() => setShowComponent3(showComponent3 ? false : true) && (() => setSelection3(isSelected3 ? false : true))}
+
+          />
+        }
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 14,
+    paddingVertical: 14,
+    flex: 1,
+    // marginTop: Constants.statusBarHeight,
+    backgroundColor: 'white'
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 5
+  },
+  header2: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    top: 10,
+    marginBottom:5
+  },
+  days: {
+    fontSize: 30,
+    top: 10, 
+    marginBottom: 15,
+    fontWeight: 'bold',
+    color:"red"
+  },
+  subheader: {
+    marginTop: 5,
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#BBBBBB",
+    marginBottom: 5
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  scrollview: {
+    paddingBottom: 40,
+  },
+
+  card: {
+    marginTop: 18,
+    paddingHorizontal: 18,
+    paddingVertical: 14,
+    backgroundColor: "#2F80ED",
+    borderRadius: 8,
+  },
+  card2: {
+    marginTop: 18,
+    paddingHorizontal: 18,
+    paddingVertical: 14,
+    backgroundColor: "#EB5757",
+    borderRadius: 8,
+  },
+  card3: {
+    marginTop: 18,
+    paddingHorizontal: 18,
+    paddingVertical: 14,
+    backgroundColor: "#9B51E0",
+    borderRadius: 8,
+  },
+  card4: {
+    marginTop: 18,
+    paddingHorizontal: 18,
+    paddingVertical: 14,
+    backgroundColor: '#219653',
+    borderRadius: 8,
+  },
+  card5: {
+    marginTop: 3,
+    paddingHorizontal: 18,
+    paddingVertical: 14,
+    backgroundColor: '#101433',
+    borderRadius: 8,
+  },
+  buttonText:
+  {
+    position: 'relative',
+    top: 5,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#2F80ED',
+    lineHeight: 19
+  },
+  buttonText2:
+  {
+    position: 'relative',
+    top: 5,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#EB5757',
+    lineHeight: 19
+  },
+  buttonText3:
+  {
+    position: 'relative',
+    top: 5,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#9B51E0',
+    lineHeight: 19
+  },
+  VotingButtonText:
+  {
+    position: 'relative',
+    top: 5,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#219653',
+    lineHeight: 19
+  },
+  CompletedCard: {
+    marginTop: 18,
+    paddingHorizontal: 18,
+    paddingVertical: 14,
+    backgroundColor: "#BBBBBB",
+    borderRadius: 8,
+  },
+  CompletedbuttonText:
+  {
+    position: 'relative',
+    top: 5,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: 'black',
+    lineHeight: 19
+  },
+});
+
